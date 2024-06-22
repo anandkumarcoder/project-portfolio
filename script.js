5475bbd725ae829ecfa7a3892817a40e1d6a6b06
Anand <akanand104@gmail.com>
2024-06-22 12:29:10 +0530

    contact form added

---
+++ script.js
@@ -0,0 +1,14 @@
+/* document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    if (name && email && message) {
+      document.getElementById('formMessage').innerText = 'Thank you for contacting us, ' + name + '!';
+      document.getElementById('contactForm').reset();
+    } else {
+      document.getElementById('formMessage').innerText = 'Please fill out all required fields.';
+    }
+  }); */
